9e22615605dd87361a24398c0f8dafd230491e31
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2013-08-20 16:39:34 +0200

    Serialize special types
    Serialize colors and urls as strings and use bytearrays for the other
    types.

--- src/configuration/qgvariantutils.cpp
+++ src/configuration/qgvariantutils.cpp
@@ -25,9 +25,9 @@
  * $END_LICENSE$
  ***************************************************************************/
 
-#include <QtCore/QRect>
-#include <QtCore/QStringList>
+#include <QDebug>
 #include <QtCore/QUrl>
+#include <QtCore/QStringList>
 
 #include "qgvariantutils.h"
 
@@ -40,44 +40,25 @@ QVariant convertValue(GVariant *value)
 
     switch (g_variant_classify(value)) {
     case G_VARIANT_CLASS_BOOLEAN:
-        return QVariant(g_variant_get_boolean(value));
+        return QVariant((bool)g_variant_get_boolean(value));
     case G_VARIANT_CLASS_BYTE:
-        return QVariant((char)g_variant_get_byte(value));
+        return QVariant(QLatin1Char(g_variant_get_byte(value)));
     case G_VARIANT_CLASS_INT16:
-        return QVariant((qint16)g_variant_get_int16(value));
+        return QVariant((int)g_variant_get_int16(value));
     case G_VARIANT_CLASS_UINT16:
-        return QVariant((quint16)g_variant_get_uint16(value));
+        return QVariant((uint)g_variant_get_uint16(value));
     case G_VARIANT_CLASS_INT32:
-        return QVariant((qint32)g_variant_get_int32(value));
+        return QVariant((int)g_variant_get_int32(value));
     case G_VARIANT_CLASS_UINT32:
-        return QVariant((quint32)g_variant_get_uint32(value));
+        return QVariant((uint)g_variant_get_uint32(value));
     case G_VARIANT_CLASS_INT64:
-        return QVariant((qint64)g_variant_get_int64(value));
+        return QVariant((qlonglong)g_variant_get_int64(value));
     case G_VARIANT_CLASS_UINT64:
-        return QVariant((quint64)g_variant_get_uint64(value));
+        return QVariant((qulonglong)g_variant_get_uint64(value));
     case G_VARIANT_CLASS_DOUBLE:
-        return QVariant((double)g_variant_get_double(value));
-    case G_VARIANT_CLASS_STRING: {
-        QString str = QString::fromUtf8(g_variant_get_string(value, NULL));
-        if (str.startsWith(QLatin1Char('@')) && str.endsWith(QLatin1Char(')'))) {
-            if (str.startsWith(QLatin1String("@Url(")))
-                return QVariant(QUrl::fromUserInput(str.mid(5, str.size() - 6)));
-            else if (str.startsWith(QLatin1String("@Variant("))) {
-#ifndef QT_NO_DATASTREAM
-                QByteArray byteArray(str.toUtf8().mid(9));
-                QDataStream stream(&byteArray, QIODevice::ReadOnly);
-                stream.setVersion(QDataStream::Qt_4_0);
-
-                QVariant result;
-                stream >> result;
-                return result;
-#else
-                Q_ASSERT("QConfiguration: cannot load custom type without QDataStream support");
-#endif
-            }
-        }
-        return QVariant(str);
-    }
+        return QVariant(g_variant_get_double(value));
+    case G_VARIANT_CLASS_STRING:
+        return QVariant(QString::fromUtf8(g_variant_get_string(value, NULL)));
     case G_VARIANT_CLASS_ARRAY:
         if (g_variant_is_of_type(value, G_VARIANT_TYPE_STRING_ARRAY)) {
             GVariantIter iter;
@@ -90,8 +71,28 @@ QVariant convertValue(GVariant *value)
                 list.append(QString::fromUtf8(str));
 
             return QVariant(list);
+        } else if (g_variant_is_of_type(value, G_VARIANT_TYPE_BYTESTRING_ARRAY)) {
+            GVariantIter iter;
+            QVariantList list;
+            const gchar *item;
+
+            g_variant_iter_init(&iter, value);
+
+            while (g_variant_iter_next(&iter, "&y", &item))
+                list.append(QByteArray(item));
+
+            return list;
+#ifndef QT_NO_DATASTREAM
         } else if (g_variant_is_of_type(value, G_VARIANT_TYPE_BYTESTRING)) {
-            return QVariant(QByteArray(g_variant_get_bytestring(value)));
+            QByteArray a(g_variant_get_bytestring(value));
+            QDataStream s(&a, QIODevice::ReadOnly);
+            s.setVersion(QDataStream::Qt_4_0);
+            QVariant result;
+            s >> result;
+            return result;
+#else
+        Q_ASSERT(!"QConfiguration: Cannot load custom types without QDataStream support");
+#endif
         }
     default:
         break;
@@ -102,99 +103,50 @@ QVariant convertValue(GVariant *value)
 
 GVariant *convertVariant(const QVariant &variant)
 {
-    switch ((QMetaType::Type)variant.type()) {
-    case QMetaType::Bool:
+    switch (variant.type()) {
+    case QVariant::Bool:
         return g_variant_new_boolean(variant.toBool());
-    case QMetaType::Char:
+    case QVariant::Char:
         return g_variant_new_byte(variant.toChar().toLatin1());
-    case QMetaType::Short: {
-        bool ok = false;
-        int value = variant.toInt(&ok);
-        if (ok)
-            return g_variant_new_int16((qint16)value);
-        return 0;
-    }
-    case QMetaType::UShort: {
-        bool ok = false;
-        uint value = variant.toUInt(&ok);
-        if (ok)
-            return g_variant_new_uint16((quint16)value);
-        return 0;
-    }
-    case QMetaType::Int:
-    case QMetaType::Long: {
-        bool ok = false;
-        int value = variant.toInt(&ok);
-        if (ok)
-            return g_variant_new_int32((qint32)value);
-        return 0;
-    }
-    case QMetaType::UInt:
-    case QMetaType::ULong: {
-        bool ok = false;
-        uint value = variant.toUInt(&ok);
-        if (ok)
-            return g_variant_new_uint32((quint32)value);
-        return 0;
-    }
-    case QMetaType::LongLong: {
-        bool ok = false;
-        qlonglong value = variant.toLongLong(&ok);
-        if (ok)
-            return g_variant_new_int64((qint64)value);
-        return 0;
-    }
-    case QMetaType::ULongLong: {
-        bool ok = false;
-        qulonglong value = variant.toULongLong(&ok);
-        if (ok)
-            return g_variant_new_int64((quint64)value);
-        return 0;
-    }
-    case QMetaType::Float: {
-        bool ok = false;
-        float value = variant.toFloat(&ok);
-        if (ok)
-            return g_variant_new_double((gdouble)value);
-    }
-    case QMetaType::Double: {
-        bool ok = false;
-        double value = variant.toDouble(&ok);
-        if (ok)
-            return g_variant_new_double(value);
-    }
-    case QMetaType::QString: {
-        QString value = variant.toString();
-        return g_variant_new_string(value.toUtf8().constData());
-    }
-    case QMetaType::QUrl: {
-        QString value = QLatin1String("@Url(") + variant.toUrl().toString() + QLatin1String(")");
-        return g_variant_new_string(value.toUtf8().constData());
-    }
-    case QMetaType::QRect: {
-        QRect value = variant.toRect();
-        GVariantBuilder *builder = g_variant_builder_new(G_VARIANT_TYPE("ai"));
-        g_variant_builder_add(builder, "i", value.x());
-        g_variant_builder_add(builder, "i", value.y());
-        g_variant_builder_add(builder, "i", value.width());
-        g_variant_builder_add(builder, "i", value.height());
-        GVariant *result = g_variant_new("ai", builder);
+    case QVariant::Int:
+        return g_variant_new_int32(variant.toInt());
+    case QVariant::UInt:
+        return g_variant_new_uint32(variant.toUInt());
+    case QVariant::LongLong:
+        return g_variant_new_int64(variant.toLongLong());
+    case QVariant::ULongLong:
+        return g_variant_new_uint64(variant.toULongLong());
+    case QVariant::Double:
+        return g_variant_new_double(variant.toDouble());
+    case QVariant::String:
+        return g_variant_new_string(variant.toString().toUtf8().constData());
+    case QVariant::StringList: {
+        QStringList value = variant.toStringList();
+
+        GVariantBuilder *builder = g_variant_builder_new(G_VARIANT_TYPE("as"));
+        foreach (QString item ,value)
+            g_variant_builder_add(builder, "s", item.toUtf8().constData());
+
+        GVariant *result = g_variant_new("as", builder);
         g_variant_builder_unref(builder);
         return result;
     }
+    case QVariant::ByteArray:
+        return g_variant_new_bytestring(variant.toByteArray().constData());
+    case QVariant::Url:
+        return g_variant_new_string(variant.toUrl().toString().toUtf8().constData());
+    case QVariant::Color:
+        return g_variant_new_string(variant.toString().toUtf8().constData());
     default: {
 #ifndef QT_NO_DATASTREAM
-        QByteArray byteArray;
-        QDataStream stream(&byteArray, QIODevice::WriteOnly);
-        stream.setVersion(QDataStream::Qt_4_0);
-        stream << variant;
-
-        QString value = QLatin1String("@Variant(");
-        value += QString::fromUtf8(byteArray.constData());
-        value += QLatin1String(")");
-        return g_variant_new_string(value.toUtf8().constData());
+        QByteArray a;
+        QDataStream s(&a, QIODevice::WriteOnly);
+        s.setVersion(QDataStream::Qt_4_0);
+        s << variant;
+        return g_variant_new_from_data(G_VARIANT_TYPE_BYTESTRING, a.constData(),
+                                       a.size(), TRUE, NULL, NULL);
 #else
-        Q_ASSERT("QConfiguration: cannot save custom types without QDataStream support");
+        Q_ASSERT(!"QConfiguration: Cannot save custom types without QDataStream support");
 #endif
     }
     }
